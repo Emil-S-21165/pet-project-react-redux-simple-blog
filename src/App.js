180b1e06c29df175b7f9906d5a75eaa39d13bd6e
EmilS <emil.s21165@gmail.com>
2023-02-03 15:41:03 +0600

    add UserPage, UserList components, project optimization

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import AddPostForm from './features/posts/AddPostForm';
 import PostsList from './features/posts/PostsList';
 import SinglePostPage from './features/posts/SinglePostPage';
 import EditPost from './features/posts/EditPost';
 import Layout from './component/Layout';
+import UsersList from './features/users/UsersList';
+import UserPage from './features/users/UserPage';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<PostsList />} />
+
         <Route path='post'>
           <Route index element={<AddPostForm />} />
           <Route path=':postId' element={<SinglePostPage />} />
           <Route path='edit/:postId' element={<EditPost />} />
         </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
 
       </Route>
     </Routes>
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,20 +1,19 @@
 import { useSelector} from "react-redux";
-import { selectAllPosts, getPostsStatus, getPostsError } from "./postsSlice";
+import { selectPostIds, getPostsStatus, getPostsError } from "./postsSlice";
 import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
 
-    const posts = useSelector(selectAllPosts);
-    const postsStatus = useSelector(getPostsStatus);
+    const orderedPostIds = useSelector(selectPostIds);
+    const postStatus = useSelector(getPostsStatus);
     const postsError = useSelector(getPostsError);
 
     let content;
-    if (postsStatus === 'loading') {
+    if (postStatus === 'loading') {
         content = <p>'Loading...'</p>;
-    } else if (postsStatus === 'succeeded') {
-        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-        content = orderedPosts.map(post => <PostsExcerpt key={post.id} post={post} />)
-    } else if (postsStatus === 'failed') {
+    } else if (postStatus === 'succeeded') {
+        content = orderedPostIds.map(postId => <PostsExcerpt key={postId} postId={postId} />)
+    } else if (postStatus === 'failed') {
         content = <p>{postsError}</p>;
     }
 
